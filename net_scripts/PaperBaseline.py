edec962daf49ca36ed0bf3781336bb4c880cf93c
MattiaMarseglia <m.marseglia1@studenti.unisa.it>
2023-07-06 19:29:13 +0200

    update_project_to_prepare_baseline_run

--- net_scripts/PaperBaseline.py
+++ net_scripts/PaperBaseline.py
@@ -1,31 +1,21 @@
 #This code is to try a single regressor with our backbone
 import os
-import cv2
-import dlib
-import json
 import torch
-import random
-import requests
 import numpy as np
 import pandas as pd
 from PIL import Image
-from tqdm import tqdm
 import torch.nn as nn
+from tqdm import tqdm
 import torch.optim as optim
-from torchscan import summary #network summary
-import torch.nn.functional as F
 from torchvision import transforms
 from torch.optim import lr_scheduler
 from torch.utils.data import Dataset
-from torch.utils.data import WeightedRandomSampler
-from torchvision.transforms import ToTensor
-from torch.utils.data import DataLoader,random_split
-from sklearn.model_selection import train_test_split
-# from torchsummary import summary #network summary
-from torchvision.models import Inception_V3_Weights, Inception3
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
+from torch.utils.data import DataLoader
 
+PARAMETERS_AND_NAME_MODEL = "Baseline EfficientNetBo"
+
+# from torchsummary import summary #network summary
+print(torch.__version__)
 print(torch.cuda.device_count())
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)     
@@ -54,206 +44,53 @@ class dataFrameDataset(Dataset):
 
         img_name = os.path.join(self.root_dir,
                                 self.df.iloc[idx, 0])
-        age = self.df.iloc[idx,1]
+        champ_class = self.df.iloc[idx,1]
         image = Image.open(img_name)
 
         if self.transform:
             image = self.transform(image)
 
-        sample = {'image': image, 'age': age}
+        sample = {'image': image, 'class': champ_class}
         return sample
 
-def preprocess_input(img):
-    return  img
+train_data = pd.read_csv(
+   '/home/mattia/Desktop/Tesi/Progetto_Tesi-1/train.csv',
+    names=["image", "class"],dtype={'image':'str','class':'str'})
+X_train = train_data["image"]
+y_train = train_data["class"]
 
-class allignment(object):
-  
-  def __init__(self, facePredictor):
-        """
-        Instantiate an 'AlignDlib' object.
-        :param facePredictor: The path to dlib's
-        :type facePredictor: str
-        """
-        assert facePredictor is not None
+val_data = pd.read_csv(
+   '/home/mattia/Desktop/Tesi/Progetto_Tesi-1/val.csv',
+    names=["image", "class"],dtype={'image':'str','class':'str'})
+X_val = val_data["image"]
+y_val = val_data["class"]
 
-        self.detector = dlib.get_frontal_face_detector()
-        self.predictor = dlib.shape_predictor(facePredictor)
-
-        self.randomBrightness = transforms.ColorJitter(brightness=(0.9,1.1))
+print(len(X_train), len(y_train))
+print(len(X_val), len(y_val))
+# X_train , X_val, y_train, y_val = train_test_split(df['image'],df['age'],train_size=0.74,random_state=2022, shuffle=True,stratify=df['age'])
 
 
-
-  def shape_to_normal(self, shape):
-      shape_normal = []
-      for i in range(0, 5):
-          shape_normal.append((i, (shape.part(i).x, shape.part(i).y)))
-      return shape_normal
-  def get_eyes_nose_dlib(self, shape):
-      nose = shape[4][1]
-      left_eye_x = int(shape[3][1][0] + shape[2][1][0]) // 2
-      left_eye_y = int(shape[3][1][1] + shape[2][1][1]) // 2
-      right_eyes_x = int(shape[1][1][0] + shape[0][1][0]) // 2
-      right_eyes_y = int(shape[1][1][1] + shape[0][1][1]) // 2
-      return nose, (left_eye_x, left_eye_y), (right_eyes_x, right_eyes_y)
-  def cosine_formula(self, length_line1, length_line2, length_line3):
-      cos_a = -(length_line3 ** 2 - length_line2 ** 2 - length_line1 ** 2) / (2 * length_line2 * length_line1)
-      return cos_a
-  def distance(self, a, b):
-      return np.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2)
-  def rotate_point(self, origin, point, angle):
-      ox, oy = origin
-      px, py = point
-
-      qx = ox + np.cos(angle) * (px - ox) - np.sin(angle) * (py - oy)
-      qy = oy + np.sin(angle) * (px - ox) + np.cos(angle) * (py - oy)
-      return qx, qy
-
-
-  def is_between(self, point1, point2, point3, extra_point):
-      c1 = (point2[0] - point1[0]) * (extra_point[1] - point1[1]) - (point2[1] - point1[1]) * (extra_point[0] - point1[0])
-      c2 = (point3[0] - point2[0]) * (extra_point[1] - point2[1]) - (point3[1] - point2[1]) * (extra_point[0] - point2[0])
-      c3 = (point1[0] - point3[0]) * (extra_point[1] - point3[1]) - (point1[1] - point3[1]) * (extra_point[0] - point3[0])
-      if (c1 < 0 and c2 < 0 and c3 < 0) or (c1 > 0 and c2 > 0 and c3 > 0):
-          return True
-      else:
-          return False
-
-  def load_image(self, path):
-    img = cv2.imread(path, 1)
-    # OpenCV loads images with color channels
-    # in BGR order. So we need to reverse them
-    return img[...,::-1]
-
-  def allignImage(self, img):       
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    rects = self.detector(gray, 0)
-    angle = 0
-    try:
-        if len(rects) > 0:
-            for rect in rects:
-                x = rect.left()
-                y = rect.top()
-                w = rect.right()
-                h = rect.bottom()
-                shape = self.predictor(gray, rect)
-            shape = self.shape_to_normal(shape)
-            nose, left_eye, right_eye = self.get_eyes_nose_dlib(shape)
-            center_of_forehead = ((left_eye[0] + right_eye[0]) // 2, (left_eye[1] + right_eye[1]) // 2)
-            center_pred = (int((x + w) / 2), int((y + y) / 2))
-            length_line1 = self.distance(center_of_forehead, nose)
-            length_line2 = self.distance(center_pred, nose)
-            length_line3 = self.distance(center_pred, center_of_forehead)
-            cos_a = self.cosine_formula(length_line1, length_line2, length_line3)
-            try:
-                angle = np.arccos(cos_a)
-            except:
-                angle = 0
-            rotated_point = self.rotate_point(nose, center_of_forehead, angle)
-            rotated_point = (int(rotated_point[0]), int(rotated_point[1]))
-            if self.is_between(nose, center_of_forehead, center_pred, rotated_point):
-                angle = np.degrees(-angle)
-            else:
-                angle = np.degrees(angle)
-    except:
-        pass
-    img = Image.fromarray(img)
-    return np.array(img.rotate(angle))
-    
-
-modelPath = ('./landmarks5.dat')
-
-allignment = allignment(modelPath)
-
-class face_alignment_train(object):
-
-    def __call__(self,img):
-        
-        if bool(random.getrandbits(1)): 
-            img = allignment.randomBrightness(img)
-
-        img = np.array(img) 
-        # Convert RGB to BGR 
-        img = allignment.allignImage(img).astype(np.uint8)
-
-        if random.random() > 0.7:
-            img = cv2.flip(img, 1)
-
-        
-        angle = 0 
-        color_converted = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img = Image.fromarray(img.astype(np.uint8))
-
-        if bool(random.getrandbits(1)): 
-            if bool(random.getrandbits(1)): 
-                angle = 5
-            else:
-                angle = -5
-        return preprocess_input(img.rotate(angle))
-
-class face_alignment_val(object):
-
-    def __call__(self,img): 
-        
-        img = np.array(img) 
-
-        img = allignment.allignImage(img).astype(np.uint8)
-        
-        return preprocess_input(img)
-
-df = pd.read_csv(
-   '/user/2022_va_gr12/training_caip_contest.csv',
-    names=["image", "age"],dtype={'image':'str','age':'float'})
-df.head()
-
-X_train , X_val, y_train, y_val = train_test_split(df['image'],df['age'],train_size=0.74,random_state=2022, shuffle=True,stratify=df['age'])
-
-df_train = pd.DataFrame({'image':X_train,'age':y_train})
-df_val = pd.DataFrame({'image':X_val,'age':y_val})
-
-
-import matplotlib.pyplot as plt
-from torch.utils.data.dataloader import default_collate
+df_train = pd.DataFrame({'image':X_train,'class':y_train})
+df_val = pd.DataFrame({'image':X_val,'class':y_val})
 
 data_transforms_train = transforms.Compose([
-transforms.Resize((160,160)), 
-face_alignment_train(),
 transforms.ToTensor(),
-transforms.Normalize([0.498, 0.498, 0.498], [0.500, 0.500, 0.500]),
+# transforms.Normalize([0.498, 0.498, 0.498], [0.500, 0.500, 0.500]),
 ])
 
 data_transforms_val = transforms.Compose([
-transforms.Resize((160,160)), 
-face_alignment_val(),
 transforms.ToTensor(),
-transforms.Normalize([0.498, 0.498, 0.498], [0.500, 0.500, 0.500]),
+# transforms.Normalize([0.498, 0.498, 0.498], [0.500, 0.500, 0.500]),
 ])
 
-PATH = "/mnt/sdc1/2022_va_gr12/training_caip_contest"
+TRAIN_PATH = "/home/mattia/Desktop/Tesi/dataset_reordered/train"
+VAL_PATH = "/home/mattia/Desktop/Tesi/dataset_reordered/validation"
 
-trainDataSet = dataFrameDataset(df_train,PATH,data_transforms_train)
-valnDataSet = dataFrameDataset(df_val,PATH,data_transforms_val)
-batch_size = 256
+trainDataSet = dataFrameDataset(df_train,TRAIN_PATH,data_transforms_train)
+valnDataSet = dataFrameDataset(df_val,VAL_PATH,data_transforms_val)
+
+batch_size = 4
 # create batches
-
-def flattening(array,flattening_coeff):
-    mean = np.mean(array)
-    diff_array = np.absolute(np.subtract(array,mean))
-    rescaled_diff_array = np.multiply(diff_array,flattening_coeff)
-    idx = array > mean
-    rescaled_diff_array[idx]*=-1
-    out = array + rescaled_diff_array
-    factor = np.divide(np.min(array),np.min(out))
-    final_arr = np.multiply(out,factor)
-    return final_arr
-
-int_ages = [int(age) for age in y_train]
-counts,bins = np.histogram(int_ages, 81) #how much samples are there for each age 
-kern = cv2.getGaussianKernel(11,1)
-filtered = np.convolve(counts, kern[:,0], mode= 'same')
-class_weights = (1/filtered)*10000
-class_weights = torch.Tensor(flattening(class_weights,0.4))
-class_weights = class_weights/torch.min(class_weights)
-
 
 train_set = DataLoader(trainDataSet,shuffle=True,batch_size=batch_size,num_workers=15)
 val_set = DataLoader(valnDataSet,shuffle=True, batch_size=batch_size,num_workers=15)
@@ -262,9 +99,9 @@ dataloaders = {'train':train_set,'val':val_set}
 dataset_sizes = {'train':len(trainDataSet),'val':len(valnDataSet)}
 
 
-class eoClassifierModel(nn.Module):
+class MyClassifierModel(nn.Module):
     def __init__(self, Backbone, LasteoClassifierModel):
-        super(eoClassifierModel, self).__init__()
+        super(MyClassifierModel, self).__init__()
         self.Backbone = Backbone
         self.LasteoClassifierModel = LasteoClassifierModel
         
@@ -275,121 +112,41 @@ class eoClassifierModel(nn.Module):
 
 #DEFINE MODEL
 
+# import torchvision.models as models
+
+# model_names = [name for name in dir(models) if not name.startswith("__")]
+# print(model_names)
+
 #BACKBONE:
-from facenet_pytorch import InceptionResnetV1
+from torchvision.models import efficientnet_b0, EfficientNet_B0_Weights
 
 # For a model pretrained on VGGFace2
-backbone = InceptionResnetV1(pretrained='vggface2')
+backbone = efficientnet_b0(weights=EfficientNet_B0_Weights.IMAGENET1K_V1)
 
-num_ftrs = backbone.last_linear.out_features
-
-#CLASSIFIER
-ClassifierModel = nn.Sequential(
-    nn.Linear(num_ftrs, num_ftrs),
-    nn.ReLU(),
-    nn.Dropout(0.1),
-    nn.Linear(num_ftrs, int(num_ftrs/2)),
-    nn.BatchNorm1d(num_features = int(num_ftrs/2)),
-    nn.ReLU(),
-    nn.Dropout(0.2),
-    nn.Linear(int(num_ftrs/2), int(num_ftrs/4)),
-    nn.BatchNorm1d(int(num_ftrs/4)),
-    nn.ReLU(),
-    nn.Dropout(0.2),
-    nn.Linear(int(num_ftrs/4), 81),
-)
 
 #FINAL MODEL
-model = eoClassifierModel(backbone, ClassifierModel)
+model = backbone
 
 count = 0
-for param in model.Backbone.parameters(): 
-    param.requires_grad = False
-    count+=1
-print(count)
-
-count = 0
-for param in model.Backbone.mixed_6a.parameters(): 
-    param.requires_grad = True
-    count+=1
-print(count)
-
-count = 0
-for param in model.Backbone.repeat_2.parameters(): 
-    param.requires_grad = True
-    count+=1
-print(count)
-
-count = 0
-for param in model.Backbone.mixed_7a.parameters(): 
-    param.requires_grad = True
-    count+=1
-print(count)
-
-count = 0
-for param in model.Backbone.repeat_3.parameters(): 
-    param.requires_grad = True
-    count+=1
-print(count)
-
-count = 0
-for param in model.Backbone.block8.parameters(): 
-    param.requires_grad = True
-    count+=1
-print(count)
-
-count = 0
-for param in model.Backbone.avgpool_1a.parameters(): 
-    param.requires_grad = True
-    count+=1
-print(count)
-
-count = 0
-for param in model.Backbone.last_linear.parameters(): 
-    param.requires_grad = True
-    count+=1
-print(count)
-
-count = 0
-for param in model.Backbone.last_bn.parameters(): 
-    param.requires_grad = True
-    count+=1
-print(count)
-
-count = 0
-for param in model.LasteoClassifierModel.parameters(): 
+for param in model.parameters(): 
     param.requires_grad = True
     count+=1
 print(count)
 
 model = model.to(device)
 
-
 model.eval()
-summary(model, (3,160,160))
-
-#considearndo il fatto dei batch bilanciati possiamo andare a calcoalre la MAE e l'AAR del batch e rendersi conto della situazione 
-def ClassificationLoss(y_pred,y_true, preds, base_criterion):
-    y_true=y_true-1
-    #return weighted_loss
-    diff = (1 + 0.35*abs(y_true-preds)).to(device)
-
-    y_true=y_true.type(torch.LongTensor).to(device)
-    return (base_criterion(y_pred,y_true)*diff).mean()
 
 # Observe that all parameters are being optimized
-optimizer = optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=1e-3, momentum=0.9)
+optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=1e-3)
 # optimizer = optim.RMSprop(filter(lambda p: p.requires_grad, model.parameters()),  lr=1e-3, alpha=0.99, eps=1e-08, weight_decay=0, momentum=0)
 # Decay LR by a factor of 0.1 every 3 epochs
-exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=10, gamma=1e-4)
+exp_lr_scheduler = lr_scheduler.ExponentialLR(optimizer, gamma=0.8)
 
-base_criterion = nn.CrossEntropyLoss(class_weights, reduction='none').to(device)
-
+base_criterion = nn.CrossEntropyLoss().to(device)
 
 import time
 import copy
-import matplotlib.pyplot as plt
-
 
 class EarlyStopper:
     def __init__(self, patience=1, min_delta=0):
@@ -419,7 +176,6 @@ def train_model(model, base_criterion, optimizer, scheduler, early_stopper,num_e
 
     toPrint = f'Ciao , sto per allenare {PARAMETERS_AND_NAME_MODEL}'
     print(toPrint)
-    possibleChoise = torch.arange(1,82).unsqueeze(0).to(device)
 
     for epoch in range(num_epochs):
         print(f'Epoch {epoch}/{num_epochs - 1}')
@@ -434,15 +190,14 @@ def train_model(model, base_criterion, optimizer, scheduler, early_stopper,num_e
 
             running_loss = 0.0
             running_corrects = 0
-            running_sigma = 0
-            running_mMAE = 0
 
             # Iterate over data.
             for sample_batched in tqdm(dataloaders[phase]):
-
+                print(type(sample_batched['image']))
+                print(type(sample_batched['class']))
                 inputs = sample_batched['image'].float().to(device)
-                labels = sample_batched['age'].float().to(device)
-
+                labels = sample_batched['class'].float().to(device)
+                
                 # zero the parameter gradients
                 optimizer.zero_grad()
 
@@ -455,7 +210,8 @@ def train_model(model, base_criterion, optimizer, scheduler, early_stopper,num_e
                     prob = nn.Softmax(dim=1)(outputs)
                     # Classifier EV: prediction = torch.sum(torch.mul(possibleChoise ,prob),dim=1) -> Classifier argmax: prediction = torch.argmax(prob,dim=1)
                     prediction = torch.argmax(prob,dim=1)
-                    loss = ClassificationLoss(outputs, labels, prediction, base_criterion)
+                    ##DA FARE LA ONE HOT ENCODING
+                    loss = base_criterion(outputs, labels)
 
                     # backward + optimize only if in training phase
                     if phase == 'train':
@@ -464,9 +220,8 @@ def train_model(model, base_criterion, optimizer, scheduler, early_stopper,num_e
 
                 # statistics
                 running_loss += loss.item()
-                prediction += 1
-                tmp = (torch.logical_and(prediction >= labels.data-1, prediction <= labels.data+1)).long()
-                running_corrects += torch.sum(tmp)
+                correctness = (torch.logical_and(prediction == labels)).long()
+                running_corrects += torch.sum(correctness)
             if phase == 'train':
                 scheduler.step()
 
--- utils/create_info_for_final_datasetCSV.py
+++ utils/create_info_for_final_datasetCSV.py
@@ -11,7 +11,16 @@ import csv
 
 All_files = []
 
-def leggi_nomi_file(cartella):
+def scrivi_su_csv(lista, nome_file):
+    with open(nome_file, 'w', newline='') as file_csv:
+        writer = csv.writer(file_csv)
+        for elemento in lista:
+            writer.writerow(elemento)
+
+# Esempio di utilizzo
+
+
+def classes_in_the_subset(cartella_base):
     i = 0
     for dirpath, dirnames, filenames in os.walk(cartella_base):
         print(i,", ",dirnames, len(filenames))
@@ -33,42 +42,71 @@ def leggi_nomi_file(cartella):
     print(len(all_set["test"]))
     return All_files
 
+def create_subset_CSV(cartella_base, class_dictionary):
+    i = 0
+    for dirpath, dirnames, filenames in os.walk(cartella_base):
+        print(i,", ",dirnames, len(filenames))
+        if i == 0:
+            folders = dirnames
+            info_set = {"train": [("image", "class")], "validation": [("image", "class")], "test": [("image", "class")]}
+        else:
+            for filename in filenames:
+                sub_strings = filename.split("-")
+                champ_class = sub_strings[1:]
+                class_string = ""
+                for elem in champ_class:
+                    class_string += elem + "-"
+                class_string = class_string.split(".")[0]
+                if class_string in class_dictionary.keys():
+                    label = class_dictionary[class_string]
+                else:
+                    print("questa non sta nel dizionario: ",class_string)
+                    input()
+
+                info_set[folders[i-1]].append((filename, label, class_string))
+        i += 1
+    print(len(info_set["train"]))
+    print(len(info_set["validation"]))
+    print(len(info_set["test"]))
+    print(len(info_set["train"]) + len(info_set["validation"]) + len(info_set["test"]))
+    return info_set
 
 # Esempio di utilizzo:
 cartella_base = "/home/mattia/Desktop/Tesi/dataset_reordered"
-All_files = leggi_nomi_file(cartella_base)
+class_dictionary = {
+    "ice":19,
+    "fresh_snow":1,
+    "melted_snow":2,
+    "wet-mud":3,
+    "wet-gravel":4,
+    "wet-asphalt-slight":5,
+    "wet-asphalt-smooth":6,
+    "wet-asphalt-severe":7,
+    "wet-concrete-slight":8,
+    "wet-concrete-smooth":9,
+    "wet-concrete-severe":10,
+    "water-mud":11,
+    "water-gravel":12,
+    "water-asphalt-slight":13,
+    "water-asphalt-smooth":14,
+    "water-asphalt-severe":15,
+    "water-concrete-slight":16,
+    "water-concrete-smooth":17,
+    "water-concrete-severe":18,
+    "dry-mud":19,
+    "dry-gravel":20,
+    "dry-asphalt-slight":21,
+    "dry-asphalt-smooth":22,
+    "dry-asphalt-severe":23,
+    "dry-concrete-slight":24,
+    "dry-concrete-smooth":25,
+    "dry-concrete-severe":26,
+}
+info_set = create_subset_CSV(cartella_base, class_dictionary)
+nome_file = './train.csv'
+scrivi_su_csv(info_set["train"], nome_file)
+nome_file = './test.csv'
+scrivi_su_csv(info_set["test"], nome_file)
+nome_file = './val.csv'
+scrivi_su_csv(info_set["validation"], nome_file)
 print("finitooo")
-# All_sorted_files = sorted(All_files, key=lambda x: x[0])
-
-# classes = {"class":{}}
-# current_date = All_sorted_files[0][1]
-# num_images = 0
-# for index, row in enumerate(All_sorted_files):
-#     print((index/len(All_sorted_files))*100,"%")
-#     if row[1] != current_date:
-#         current_date = row[1]
-#     sub_strings = row[0].split("-")
-#     if str(sub_strings[1:]) not in classes["class"].keys():
-#         classes["class"][str(sub_strings[1:])] =  [0, current_date]
-#     else:
-#         if current_date not in classes["class"][str(sub_strings[1:])][1].split(","):
-#             classes["class"][str(sub_strings[1:])][1] += "," + current_date
-#         classes["class"][str(sub_strings[1:])][0] += 1
-#     num_images += 1
-# filename = '/home/mattia/Desktop/Tesi/info.csv'
-# with open(filename, 'w', newline='') as csvfile:
-#     writer = csv.writer(csvfile)
-#     for c, v in classes["class"].items():
-#         writer.writerow([c, str(v)])
-# print(num_images)
-########### UNCOMMENT TO CREATE THE CSV image name, ordered date date ###########
-# filename = '/home/mattia/Desktop/Tesi/data.csv'
-# with open(filename, 'w', newline='') as csvfile:
-    # writer = csv.writer(csvfile)
-    # for item in All_sorted_files:
-        # writer.writerow([item,item[:4]+"-"+item[4:6]+"-"+item[6:8]])
-
-# print(len(All_files))
-# print(All_files[0])
-# print(All_files[-1])
-
